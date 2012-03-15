c53b71b3361b3f0676c00d60388526914634b69f
Brian Carstensen <brian.carstensen@gmail.com>
2012-03-15 14:03:17 -0500

    Work on portable Zooniverse bar

--- zooniverse-bar/ZooniverseBar.coffee
+++ zooniverse-bar/ZooniverseBar.coffee
@@ -1,101 +1,146 @@
+define = window.define or (factory) ->
+  getGlobal = (module) -> window[module]
+  window.ZooniverseBar = factory getGlobal, null
+
 define (require, exports) ->
 	$ = require 'jQuery'
 
-	translate = (raw, href) ->
-		out = if raw.constructor is String
+	translate = (raw) ->
+		if raw.constructor is String
 			"<span>#{raw}</span>"
 		else
 			("<span lang='#{lang}'>#{string}</span>" for lang, string of raw).join ''
 
-		out = "<a href='#{href}'>#{out}</a>" if href?
-
-		out
 
 	class Menu
 		constructor: (options) ->
-			@el = $("<ul></ul>")
-			@el.append option.el for option in options
+			@el = $('<ul class="menu"></ul>')
+			for option in options
+				@el.append option.el
+				option.el.addClass 'menu-item'
 
 	class Link
 		constructor: (text, href) ->
-			@el = $("<li>#{translate text, href}</li>")
+			@el = $("<li class='link'><a href='#{href}'>#{translate text}</a></li>")
 
 	class Dropdown
 		constructor: (heading, content) ->
-			@el = $("<li></li>")
-			if heading.constructor is
-				@el.append "<span>#{translate heading}</span>"
+			@el = $('<li class="dropdown"></li>')
+			if heading.constructor is Object
+				@el.append "<span class='dropdown-heading'>#{translate heading}</span>"
 			else if heading.el?
 				@el.append heading.el
+				heading.el.addClass 'dropdown-heading'
 
 			if content.constructor is String
-				@el.append "<div>#{content}</div>"
+				@el.append "<div class='dropdown-content'>#{content}</div>"
 			else if content.el?
 				@el.append content.el
+				content.el.addClass 'dropdown-content'
 
-	class Accordion extends Dropdown
-		constructor: ->
-			super
-			@el.addClass 'accordion'
+			@el.children().first().on 'click', @onClickHeading
+
+		onClickHeading: (e) =>
+			wasOpen = @el.hasClass 'open'
+			@el.closest('.zooniverse-bar').find('.dropdown').removeClass 'open'
+			@el.addClass 'open' unless wasOpen
+
+	class Accordion
+		constructor: (heading, content) ->
+			@el = $('<li class="accordion"></li>')
+			if heading.constructor is Object
+				@el.append "<span class='accordion-heading'>#{translate heading}</span>"
+			else if heading.el?
+				@el.append heading.el
+				heading.el.addClass 'accordion-heading'
+
+			if content.constructor is String
+				@el.append "<div class='accordion-content'>#{content}</div>"
+			else if content.el?
+				@el.append content.el
+				content.el.addClass 'accordion-content'
+
+			@el.children().first().on 'click', @onClickHeading
+
+		onClickHeading: (e) =>
+			@el.siblings('.accordion').removeClass 'open'
+			@el.toggleClass 'open'
 
 
-	home = new Link
-		en: 'Zooniverse', de: 'Zooniverse', pl: 'Zooniverse'
-		'http://zooniverse.org/'
+	items =
+		home: new Link
+			en: 'Zooniverse', de: 'Zooniverse', pl: 'Zooniverse'
+			'http://zooniverse.org/'
 
-	languages = new Dropdown
-		en: 'EN', de: 'DE', pl: 'PL'
-		new Menu [
-			new Link 'English', '#language:en'
-			new Link 'Deutsch', '#language:de'
-			new Link 'Polski', '#language:pl'
-		]
+		languages: new Dropdown
+			en: 'EN', de: 'DE', pl: 'PL'
+			new Menu [
+				new Link 'English', '#language:en'
+				new Link 'Deutsch', '#language:de'
+				new Link 'Polski', '#language:pl'
+			]
 
-	about = new Link
-		en: 'About', de: 'About', pl: 'About'
-		'http://zooniverse.org/about'
+		about: new Link
+			en: 'About', de: 'About', pl: 'About'
+			'http://zooniverse.org/about'
 
-	projects = new Dropdown
-		en: 'Projects', de: 'Projects', pl: 'Projects'
-		new Menu [
-			new Accordion
-				en: 'Space', de: 'Space', pl: 'Space'
-				new Menu [
-					new Link 'Galaxy Zoo: Hubble', '#'
-					new Link 'Galaxy Zoo: Mergers', '#'
-					new Link 'Galaxy Zoo: Supernovae', '#'
-				]
-			new Accordion
-				en: 'Climate', de: 'Climate', pl: 'Climate'
-				new Menu [
-					new Link 'Old Weather': '#'
-				]
-			new Accordion
-				en: 'Humanities', de: 'Humanities', pl: 'Humanities'
-				new Menu [
-					new Link 'Ancient Lives', '#'
-				]
-			new Accordion
-				en: 'Nature', de: 'Nature', pl: 'Nature'
-				new Menu [
-					new Link 'Whale FM', '#'
-				]
-		]
+		projects: new Dropdown
+			en: 'Projects', de: 'Projects', pl: 'Projects'
+			new Menu [
+				new Accordion
+					en: 'Space', de: 'Space', pl: 'Space'
+					new Menu [
+						new Link 'Galaxy Zoo: Hubble', '#'
+						new Link 'Galaxy Zoo: Mergers', '#'
+						new Link 'Galaxy Zoo: Supernovae', '#'
+					]
+				new Accordion
+					en: 'Climate', de: 'Climate', pl: 'Climate'
+					new Menu [
+						new Link 'Old Weather', '#'
+					]
+				new Accordion
+					en: 'Humanities', de: 'Humanities', pl: 'Humanities'
+					new Menu [
+						new Link 'Ancient Lives', '#'
+					]
+				new Accordion
+					en: 'Nature', de: 'Nature', pl: 'Nature'
+					new Menu [
+						new Link 'Whale FM', '#'
+					]
+			]
 
-	login = new Dropdown
-		en: 'Log in', de: 'Log in', pl: 'Log in'
-	  '''
-			<form>TODO</form>
-		'''
-
-	leading = new Menu [home, languages]
-	leading.el.addClass 'leading'
-
-	trailing = new Menu [about, projects, login]
-	trailing.el.addClass 'trailing'
+		signIn: new Dropdown
+			en: 'Sign in', de: 'Sign in', pl: 'Sign in'
+		  '''
+				<form class="sign-in">
+					<div>
+						<label>
+							<span>Username</span>
+							<input type="text" name="username" />
+						</label>
+					</div>
+					<div>
+						<label>
+							<span>Password</span>
+							<input type="password" name="password" />
+						</label>
+					</div>
+					<div class="action">
+						<a href="#">Create a new account</a>
+						<button type="submit">Sign in</button>
+					</div>
+				</form>
+			'''
 
 
 	class ZooniverseBar
+		@Menu = Menu
+		@Link = Link
+		@Dropdown = Dropdown
+		@Accordion = Accordion
+
 		constructor: (params) ->
 			@[property] = params[property] for own property of params or {}
 
@@ -103,10 +148,12 @@ define (require, exports) ->
 			@el = $(@el) unless @el.constructor is $
 			@el.addClass 'zooniverse-bar'
 
-			@el.append leading.el
-			@el.append trailing.el
+			for direction in ['leading', 'trailing']
+				group = new Menu (items[name] for name in @[direction] when name of items)
+				group.el.addClass direction
+				@el.append group.el
 
-			defaultLang = @el.parent('[lang]').attr 'lang'
+			defaultLang = @el.closest('[lang]').attr 'lang'
 			@el.attr 'lang', defaultLang or 'en'
 
 			@delegateEvents()
@@ -114,7 +161,17 @@ define (require, exports) ->
 		delegateEvents: =>
 			@el.on 'click', '[href^="#language:"]', (e) =>
 				e.preventDefault()
-				lang = $(e.target).parent('[href^="#language:"]').attr('href').split(':')[1]
+				lang = $(e.target).closest('[href^="#language:"]').attr('href').split(':')[1]
 				@el.attr 'lang', lang
+				@closeAllDropdowns()
+
+			@el.on 'click', (e) => e.stopPropagation();
+			$(document).on 'click', ':not(.zooniverse-bar *)', @closeAllDropdowns
+
+		closeAllDropdowns: =>
+			@el.find('.dropdown.open').removeClass 'open'
 
 	exports = ZooniverseBar
+
+	# This explicitly for the faux-define function.
+	return ZooniverseBar
--- zooniverse-bar/ZooniverseBar.js
+++ zooniverse-bar/ZooniverseBar.js
@@ -1,33 +1,44 @@
 (function() {
-  var __hasProp = Object.prototype.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; },
-    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  var define,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __hasProp = Object.prototype.hasOwnProperty;
+
+  define = window.define || function(factory) {
+    var getGlobal;
+    getGlobal = function(module) {
+      return window[module];
+    };
+    return window.ZooniverseBar = factory(getGlobal, null);
+  };
 
   define(function(require, exports) {
-    var $, Accordion, Dropdown, Link, Menu, ZooniverseBar, about, home, languages, leading, login, projects, trailing, translate;
+    var $, Accordion, Dropdown, Link, Menu, ZooniverseBar, items, translate;
     $ = require('jQuery');
-    translate = function(raw, href) {
-      var lang, out, string;
-      out = raw.constructor === String ? "<span>" + raw + "</span>" : ((function() {
-        var _results;
-        _results = [];
-        for (lang in raw) {
-          string = raw[lang];
-          _results.push("<span lang='" + lang + "'>" + string + "</span>");
-        }
-        return _results;
-      })()).join('');
-      if (href != null) out = "<a href='" + href + "'>" + out + "</a>";
-      return out;
+    translate = function(raw) {
+      var lang, string;
+      if (raw.constructor === String) {
+        return "<span>" + raw + "</span>";
+      } else {
+        return ((function() {
+          var _results;
+          _results = [];
+          for (lang in raw) {
+            string = raw[lang];
+            _results.push("<span lang='" + lang + "'>" + string + "</span>");
+          }
+          return _results;
+        })()).join('');
+      }
     };
     Menu = (function() {
 
       function Menu(options) {
         var option, _i, _len;
-        this.el = $("<ul></ul>");
+        this.el = $('<ul class="menu"></ul>');
         for (_i = 0, _len = options.length; _i < _len; _i++) {
           option = options[_i];
           this.el.append(option.el);
+          option.el.addClass('menu-item');
         }
       }
 
@@ -37,7 +48,7 @@
     Link = (function() {
 
       function Link(text, href) {
-        this.el = $("<li>" + (translate(text, href)) + "</li>");
+        this.el = $("<li class='link'><a href='" + href + "'>" + (translate(text)) + "</a></li>");
       }
 
       return Link;
@@ -46,88 +57,118 @@
     Dropdown = (function() {
 
       function Dropdown(heading, content) {
-        this.el = $("<li></li>");
-        if (heading.constructor === this.el.append("<span>" + (translate(heading)) + "</span>")) {} else if (heading.el != null) {
+        this.onClickHeading = __bind(this.onClickHeading, this);        this.el = $('<li class="dropdown"></li>');
+        if (heading.constructor === Object) {
+          this.el.append("<span class='dropdown-heading'>" + (translate(heading)) + "</span>");
+        } else if (heading.el != null) {
           this.el.append(heading.el);
+          heading.el.addClass('dropdown-heading');
         }
         if (content.constructor === String) {
-          this.el.append("<div>" + content + "</div>");
+          this.el.append("<div class='dropdown-content'>" + content + "</div>");
         } else if (content.el != null) {
           this.el.append(content.el);
+          content.el.addClass('dropdown-content');
         }
+        this.el.children().first().on('click', this.onClickHeading);
       }
+
+      Dropdown.prototype.onClickHeading = function(e) {
+        var wasOpen;
+        wasOpen = this.el.hasClass('open');
+        this.el.closest('.zooniverse-bar').find('.dropdown').removeClass('open');
+        if (!wasOpen) return this.el.addClass('open');
+      };
 
       return Dropdown;
 
     })();
-    Accordion = (function(_super) {
+    Accordion = (function() {
 
-      __extends(Accordion, _super);
-
-      function Accordion() {
-        Accordion.__super__.constructor.apply(this, arguments);
-        this.el.addClass('accordion');
+      function Accordion(heading, content) {
+        this.onClickHeading = __bind(this.onClickHeading, this);        this.el = $('<li class="accordion"></li>');
+        if (heading.constructor === Object) {
+          this.el.append("<span class='accordion-heading'>" + (translate(heading)) + "</span>");
+        } else if (heading.el != null) {
+          this.el.append(heading.el);
+          heading.el.addClass('accordion-heading');
+        }
+        if (content.constructor === String) {
+          this.el.append("<div class='accordion-content'>" + content + "</div>");
+        } else if (content.el != null) {
+          this.el.append(content.el);
+          content.el.addClass('accordion-content');
+        }
+        this.el.children().first().on('click', this.onClickHeading);
       }
+
+      Accordion.prototype.onClickHeading = function(e) {
+        this.el.siblings('.accordion').removeClass('open');
+        return this.el.toggleClass('open');
+      };
 
       return Accordion;
 
-    })(Dropdown);
-    home = new Link({
-      en: 'Zooniverse',
-      de: 'Zooniverse',
-      pl: 'Zooniverse'
-    }, 'http://zooniverse.org/');
-    languages = new Dropdown({
-      en: 'EN',
-      de: 'DE',
-      pl: 'PL'
-    }, new Menu([new Link('English', '#language:en'), new Link('Deutsch', '#language:de'), new Link('Polski', '#language:pl')]));
-    about = new Link({
-      en: 'About',
-      de: 'About',
-      pl: 'About'
-    }, 'http://zooniverse.org/about');
-    projects = new Dropdown({
-      en: 'Projects',
-      de: 'Projects',
-      pl: 'Projects'
-    }, new Menu([
-      new Accordion({
-        en: 'Space',
-        de: 'Space',
-        pl: 'Space'
-      }, new Menu([new Link('Galaxy Zoo: Hubble', '#'), new Link('Galaxy Zoo: Mergers', '#'), new Link('Galaxy Zoo: Supernovae', '#')])), new Accordion({
-        en: 'Climate',
-        de: 'Climate',
-        pl: 'Climate'
+    })();
+    items = {
+      home: new Link({
+        en: 'Zooniverse',
+        de: 'Zooniverse',
+        pl: 'Zooniverse'
+      }, 'http://zooniverse.org/'),
+      languages: new Dropdown({
+        en: 'EN',
+        de: 'DE',
+        pl: 'PL'
+      }, new Menu([new Link('English', '#language:en'), new Link('Deutsch', '#language:de'), new Link('Polski', '#language:pl')])),
+      about: new Link({
+        en: 'About',
+        de: 'About',
+        pl: 'About'
+      }, 'http://zooniverse.org/about'),
+      projects: new Dropdown({
+        en: 'Projects',
+        de: 'Projects',
+        pl: 'Projects'
       }, new Menu([
-        new Link({
-          'Old Weather': '#'
-        })
-      ])), new Accordion({
-        en: 'Humanities',
-        de: 'Humanities',
-        pl: 'Humanities'
-      }, new Menu([new Link('Ancient Lives', '#')])), new Accordion({
-        en: 'Nature',
-        de: 'Nature',
-        pl: 'Nature'
-      }, new Menu([new Link('Whale FM', '#')]))
-    ]));
-    login = new Dropdown({
-      en: 'Log in',
-      de: 'Log in',
-      pl: 'Log in'
-    }, '<form>TODO</form>');
-    leading = new Menu([home, languages]);
-    leading.el.addClass('leading');
-    trailing = new Menu([about, projects, login]);
-    trailing.el.addClass('trailing');
+        new Accordion({
+          en: 'Space',
+          de: 'Space',
+          pl: 'Space'
+        }, new Menu([new Link('Galaxy Zoo: Hubble', '#'), new Link('Galaxy Zoo: Mergers', '#'), new Link('Galaxy Zoo: Supernovae', '#')])), new Accordion({
+          en: 'Climate',
+          de: 'Climate',
+          pl: 'Climate'
+        }, new Menu([new Link('Old Weather', '#')])), new Accordion({
+          en: 'Humanities',
+          de: 'Humanities',
+          pl: 'Humanities'
+        }, new Menu([new Link('Ancient Lives', '#')])), new Accordion({
+          en: 'Nature',
+          de: 'Nature',
+          pl: 'Nature'
+        }, new Menu([new Link('Whale FM', '#')]))
+      ])),
+      signIn: new Dropdown({
+        en: 'Sign in',
+        de: 'Sign in',
+        pl: 'Sign in'
+      }, '<form class="sign-in">\n	<div>\n		<label>\n			<span>Username</span>\n			<input type="text" name="username" />\n		</label>\n	</div>\n	<div>\n		<label>\n			<span>Password</span>\n			<input type="password" name="password" />\n		</label>\n	</div>\n	<div class="action">\n		<a href="#">Create a new account</a>\n		<button type="submit">Sign in</button>\n	</div>\n</form>')
+    };
     ZooniverseBar = (function() {
 
+      ZooniverseBar.Menu = Menu;
+
+      ZooniverseBar.Link = Link;
+
+      ZooniverseBar.Dropdown = Dropdown;
+
+      ZooniverseBar.Accordion = Accordion;
+
       function ZooniverseBar(params) {
+        this.closeAllDropdowns = __bind(this.closeAllDropdowns, this);
         this.delegateEvents = __bind(this.delegateEvents, this);
-        var defaultLang, property, _ref;
+        var defaultLang, direction, group, name, property, _i, _len, _ref, _ref2;
         _ref = params || {};
         for (property in _ref) {
           if (!__hasProp.call(_ref, property)) continue;
@@ -136,27 +177,51 @@
         this.el || (this.el = $('<div></div>'));
         if (this.el.constructor !== $) this.el = $(this.el);
         this.el.addClass('zooniverse-bar');
-        this.el.append(leading.el);
-        this.el.append(trailing.el);
-        defaultLang = this.el.parent('[lang]').attr('lang');
+        _ref2 = ['leading', 'trailing'];
+        for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
+          direction = _ref2[_i];
+          group = new Menu((function() {
+            var _j, _len2, _ref3, _results;
+            _ref3 = this[direction];
+            _results = [];
+            for (_j = 0, _len2 = _ref3.length; _j < _len2; _j++) {
+              name = _ref3[_j];
+              if (name in items) _results.push(items[name]);
+            }
+            return _results;
+          }).call(this));
+          group.el.addClass(direction);
+          this.el.append(group.el);
+        }
+        defaultLang = this.el.closest('[lang]').attr('lang');
         this.el.attr('lang', defaultLang || 'en');
         this.delegateEvents();
       }
 
       ZooniverseBar.prototype.delegateEvents = function() {
         var _this = this;
-        return this.el.on('click', '[href^="#language:"]', function(e) {
+        this.el.on('click', '[href^="#language:"]', function(e) {
           var lang;
           e.preventDefault();
-          lang = $(e.target).parent('[href^="#language:"]').attr('href').split(':')[1];
-          return _this.el.attr('lang', lang);
+          lang = $(e.target).closest('[href^="#language:"]').attr('href').split(':')[1];
+          _this.el.attr('lang', lang);
+          return _this.closeAllDropdowns();
         });
+        this.el.on('click', function(e) {
+          return e.stopPropagation();
+        });
+        return $(document).on('click', ':not(.zooniverse-bar *)', this.closeAllDropdowns);
+      };
+
+      ZooniverseBar.prototype.closeAllDropdowns = function() {
+        return this.el.find('.dropdown.open').removeClass('open');
       };
 
       return ZooniverseBar;
 
     })();
-    return exports = ZooniverseBar;
+    exports = ZooniverseBar;
+    return ZooniverseBar;
   });
 
 }).call(this);
